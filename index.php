89564b1eb3a94fc9442c840e5a4bcd4964c034d7
TuckerFerguson <tuckerferguson@u.boisestate.edu>
2019-10-16 14:54:46 -0600

    stacy

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <div id="bg"></div>
+    <header>
+        <a href="#">Portfolio Documents</a>
+    </header>
+        <div class="centered">
+            <div class="profpic">
+                <img src="profilePic.png">
+            </div>
+            <div class="biobanner">
+              <h1>Tucker J.</h1>
+              <h2>Ferguson</h2>
+            </div>
+            <div class="bio">
+               <h1>About Me</h1>
+                 <ul>
+                  <li>Dev</li>
+                  <li>Resume</li>
+                  <li>Bio</li>
+                  <li>Evener</li>
+                  <li>Hobbys</li>
+                </ul> 
+            </div>
+            <div class="scroller">
+            </div>
+           </div> 
+    <footer>
+        <div id="outro"><p>Thanks for Visitng</p></div>
+        <div id="social"><p>Follow:</p>
+           <a href="https://www.facebook.com/tucker.ferguson.7?ref=bookmarks"><img src="Facebook.png"></a>
+           <a href="https://github.com/TuckerFerguson"><img src="Github.png"></a>
+           <a href="https://www.linkedin.com/in/tucker-ferguson-04521511a/"><img src="Linkedin.png"></a>
+        </div>
+        <div id="message"><p>Message Me</p>
+          <button id="message" onclick="message()">
+              <span></span>
+          </button>
+        </div>
+      </footer>
+</body>
+
+</html>
